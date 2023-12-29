73d39a71449b8ad81235784c67d9af6671a1418d
Pranav Kumar <pranavkumar2025.pmd@gmail.com>
2023-12-29 17:11:58 +0530

    OOps Commit

---
+++ OOps/Object_And_Classes.java
@@ -0,0 +1,67 @@
+package OOps;
+
+/**
+ * myfile
+ */
+class Object_And_Classes {
+  public static void main(String[] args) {
+
+    // create objects led and halogen
+    Lamp led = new Lamp();
+    Lamp halogen = new Lamp();
+
+    // turn on the light by
+    // calling method turnOn()
+    led.turnOn();
+
+    // turn off the light by
+    // calling method turnOff()
+    halogen.turnOff();
+  }
+}
+class Lamp {
+  
+  // stores the value for light
+  // true if light is on
+  // false if light is off
+  boolean isOn;
+
+  // method to turn on the light
+  void turnOn() {
+    isOn = true;
+    System.out.println("Light on? " + isOn);
+
+  }
+
+  // method to turnoff the light
+  void turnOff() {
+    isOn = false;
+    System.out.println("Light on? " + isOn);
+  }
+}
+
+
+// ------------------------------------------Some easy way of Class and Object Code
+
+
+// class Main {
+
+//   // create a method
+//   public int addNumbers(int a, int b) {
+//     int sum = a + b;
+//     // return value
+//     return sum;
+//   }
+
+//   public static void main(String[] args) {
+    
+//     int num1 = 25;
+//     int num2 = 15;
+
+//     // create an object of Main
+//     Main obj = new Main();
+//     // calling method
+//     int result = obj.addNumbers(num1, num2);
+//     System.out.println("Sum is: " + result);
+//   }
+// }
